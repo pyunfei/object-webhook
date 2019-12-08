88505cefa68afb9b4c32d09e894a2ba2ff50799b
pengyunfei <stack_admin@163.com>
2019-12-08 17:22:28 +0800

    s

--- webhook.sh
+++ webhook.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "拉取server最新代码"
 git pull origin master
 echo "开始构建镜像"
-docker build -t object-server .
+docker build -t object-server:1.0 .
 echo "删除旧容器"
 docker stop object-server-container
 docker rm object-server-container
 echo "启动新容器"
-docker container run -p 3000:3000 -d --name object-server-container -d object-server
+docker container run -p 3000:3000 -d --name object-server-container -d object-server:1.0
