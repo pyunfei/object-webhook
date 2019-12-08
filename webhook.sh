e1c13d66569f09fecdab9e4b73f1f22caa04da77
pengyunfei <stack_admin@163.com>
2019-12-08 17:15:17 +0800

    s

---
+++ webhook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORK_PATH='/usr/poject/object-server'
+cd $WORK_PATH
+echo "清理server代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取server最新代码"
+git pull origin master
+echo "开始构建镜像"
+docker build -t object-server .
+echo "删除旧容器"
+docker stop object-server-container
+docker rm object-server-container
+echo "启动新容器"
+docker container run -p 3000:3000 -d --name object-server-container -d object-server
